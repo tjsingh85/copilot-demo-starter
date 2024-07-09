051917b9eaf086af5bbebc1c5bccd5432122c6a7
tjsingh85 <tjsingh85@github.com>
2024-07-09 18:13:34 +0200

    added new files

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join('index.html'));
+    res.sendFile(path.join(__dirname, 'index.html')); // Corrected path
 });
 
 app.listen(port, () => {
